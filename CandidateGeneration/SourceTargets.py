41b6a9314582b76aed043959692a1dd8b4e85297
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-09-15 11:51:51 +0200

    creation and deletion of files

---
+++ CandidateGeneration/SourceTargets.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def SourceTargetsDoc(a):
+    '''The module generates distant annotations for PICO entities using a cominbation of distant supervision and dynamic programming'''
+    return a**a
+
+print( SourceTargetsDoc.__doc__ )
+
+__author__ = "Anjani Dhrangadhariya"
+__maintainer__ = "Anjani Dhrangadhariya"
+__email__ = "anjani.dhrangadhariya@hevs.ch"
+__status__ = "Prototype"
+
