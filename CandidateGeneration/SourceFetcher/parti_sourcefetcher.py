388662db217d9723a28bedc2dd0a5a497a0cf8bb
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-09-24 17:13:11 +0200

    updated

---
+++ CandidateGeneration/SourceFetcher/parti_sourcefetcher.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+def SourceFetcherDoc(a):
+    '''The module retrieves sources for each of the P - participant entity'''
+    return a**a
+
+print( SourceFetcherDoc.__doc__ )
+
+
+'''
+Description:
+    Extracts the list of participant conditions for the NCT record
+
+Args:
+    json_document (json): JSON string containing the protocol_section of a NCT record
+
+Returns:
+    dictionary: a list of strings representing the participant condition names/terms ['', '', '']
+'''
+def fetchParticipantCondition(json_document):
+
+    conditionList = []
+
+    if 'ConditionsModule' in json_document:
+        if 'ConditionList' in json_document['ConditionsModule']:
+            conditionList = json_document['ConditionsModule']['ConditionList']['Condition']
+
+    return conditionList
+
+'''
+Description:
+    Extracts the string of gender/sex of the participants in the clinical trial from the NCT record
+
+Args:
+    json_document (json): JSON string containing the protocol_section of a NCT record
+
+Returns:
+    string: a string representing the participant condition names/terms.  string ⊂ ['all', 'Male', 'Female']
+'''
+def fetchParticipantGender(json_document):
+
+    if 'EligibilityModule' in json_document:
+        if 'Gender' in json_document['EligibilityModule']:
+            gender = json_document['EligibilityModule']['Gender']
+
+    return gender
+
+'''
+Description:
+    Extracts the dictionary of age information of the participants in the clinical trial from the NCT record
+
+Args:
+    json_document (json): JSON string containing the protocol_section of a NCT record
+        (default is False)
+
+Returns:
+    dictionary: a dictionary containing minimum and maximum participant ages and standardized MeSH age groups
+'''
+def fetchParticipantAge(json_document):
+
+    stdAge = dict()
+
+    if 'EligibilityModule' in json_document:
+        if 'StdAgeList' in json_document['EligibilityModule']:
+            stdAge = json_document['EligibilityModule']['StdAgeList']
+        if 'MinimumAge' in json_document['EligibilityModule']:
+            minAge = json_document['EligibilityModule']['MinimumAge']
+            stdAge['MinimumAge'] = minAge
+        if 'MaximumAge' in json_document['EligibilityModule']:
+            maxAge = json_document['EligibilityModule']['MaximumAge']
+            stdAge['MaximumAge'] = maxAge
+
+    return stdAge # should be tuple of name
+
+'''
+Description:
+    Extracts the string of sample size information of the participants in the clinical trial from the NCT record
+
+Args:
+    json_document (json): JSON string containing the protocol_section of a NCT record
+        (default is False)
+
+Returns:
+    string: a string containing sample size information in the clinical trials
+'''
+def fetchParticipantSampSize(json_document):
+
+    if 'DesignModule' in json_document:
+            if 'EnrollmentInfo' in json_document['DesignModule']:
+                sampleSize = json_document['DesignModule']['EnrollmentInfo']['EnrollmentCount']
+
+    return sampleSize 
+
+
+'''
+Description:
+    Fetches participant (P) subtype sources from the NCT record
+
+Args:
+    json_document (json): JSON string containing the protocol_section of a NCT record
+        (default is False)
+
+Returns:
+    dictionary: a dictionary containing all the participant subtype sources
+'''
+def fetchParticipantSources(json_document):
+
+    combined_sources = dict()
+
+    p_condition = fetchParticipantCondition(json_document)
+    p_age = fetchParticipantAge(json_document)
+    p_gender = fetchParticipantGender(json_document)
+    p_sampsize = fetchParticipantSampSize(json_document)
+
+    combined_sources['condition'] = p_condition
+    combined_sources['age'] = p_age
+    combined_sources['gender'] = p_gender
+    combined_sources['sample_size'] = p_sampsize
+
+    return combined_sources
--- CandidateGeneration/label_generator.py
+++ CandidateGeneration/label_generator.py
@@ -11,3 +11,87 @@ __maintainer__ = "Anjani Dhrangadhariya"
 __email__ = "anjani.dhrangadhariya@hevs.ch"
 __status__ = "Prototype"
 
+import sys, json, os
+import logging
+import datetime as dt
+import time
+import random 
+
+from elasticsearch import Elasticsearch, helpers
+from elasticsearch_dsl import Search,  Q
+
+import difflib, re
+import nltk
+from nltk.tokenize import WhitespaceTokenizer, sent_tokenize, word_tokenize
+
+from collections import Counter
+from collections import defaultdict
+import collections
+import numpy as np
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from pylab import *
+
+# Import DISANT-PICO modules
+from SourceFetcher.parti_sourcefetcher import *
+
+
+################################################################################
+# Set the logger here
+################################################################################
+LOG_FILE = os.getcwd() + "/logs"
+if not os.path.exists(LOG_FILE):
+    os.makedirs(LOG_FILE)
+
+LOG_FILE = LOG_FILE + "/" + dt.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d %H_%M_%S') + ".log"
+logFormatter = logging.Formatter("%(levelname)s %(asctime)s %(processName)s %(message)s")
+fileHandler = logging.FileHandler("{0}".format(LOG_FILE))
+rootLogger = logging.getLogger()
+rootLogger.addHandler(fileHandler)
+rootLogger.setLevel(logging.INFO)
+
+################################################################################
+# Instantiate ElasticSearch
+################################################################################
+es = Elasticsearch( [{u'host': u'127.0.0.1', u'port': b'9200'}] )
+
+################################################################################
+# Get all the documents from the index
+################################################################################
+# Scan all of the CTO index (This index stores all the documents downloaded from clinicaltrials.org)
+results_gen = helpers.scan(
+    es,
+    query={"query": {"match_all": {}}},
+    index='ctofull-index',
+    size=1000,
+    scroll="60m",
+)
+
+match_scores = []
+intervention_types = []
+
+res = es.search(index="ctofull2021-index", body={"query": {"match_all": {}}}, size=500)
+print('Total number of records retrieved: ', res['hits']['total']['value'])
+
+# Iterate through all of the fetched CTO index documents
+# for hit in results_gen: # XXX: Entire CTO
+for n, hit in enumerate( res['hits']['hits'] ): # XXX: Only a part search results from the CTO
+
+    write_hit = collections.defaultdict(dict) # Dictionary to store the annotations for the CTO record being iterated
+
+    fullstudy = hit['_source']['FullStudiesResponse']['FullStudies'][0]['Study']
+    NCT_id = hit['_source']['FullStudiesResponse']['Expression']
+    write_hit['id'] = NCT_id
+
+    try:
+
+        protocol_section = fullstudy['ProtocolSection']
+        derieved_section = fullstudy['DerivedSection']
+
+        participants = fetchParticipantSources(protocol_section)
+
+    except:
+        logNCTID = 'Caused exception at the NCT ID: ' + NCT_id
+        logging.info(logNCTID)
