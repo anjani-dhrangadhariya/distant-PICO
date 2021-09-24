b64cecc108f56ab7614c107980f6d9dfd32da7b8
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-09-24 18:01:31 +0200

    added intervention fetcher

--- CandidateGeneration/SourceFetcher/int_sourcefetcher.py
+++ CandidateGeneration/SourceFetcher/int_sourcefetcher.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
 def IntSourceFetcherDoc(a):
-    '''The module retrieves sources for each of the I - Intervention entity'''
+    '''The module retrieves sources for each of the I - Intervention and C - Comparator entities'''
     return a**a
 
 print( IntSourceFetcherDoc.__doc__ )
+
+
+def fetchIntcompSources(json_document):
+
+    combined_sources = dict()
+
+    p_condition = fetchParticipantCondition(json_document)
+
+    return combined_sources
