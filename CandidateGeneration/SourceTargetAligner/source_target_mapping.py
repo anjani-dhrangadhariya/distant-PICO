596232c351c8441bec8574a933b1903a1b934dfc
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-06 15:57:56 +0200

    added new mapping

--- CandidateGeneration/SourceTargetAligner/source_target_mapping.py
+++ CandidateGeneration/SourceTargetAligner/source_target_mapping.py
@@ -5,5 +5,8 @@ def generateMapping():
     mapping = dict()
 
     mapping['ep_sample_size'] =  ['BriefSummary', 'DetailedDescription']
+    mapping['ep_gender'] =  ['BriefSummary', 'DetailedDescription']
+    mapping['ep_age'] =  ['BriefSummary', 'DetailedDescription']
+    mapping['ep_condition'] =  ['BriefSummary', 'DetailedDescription']
 
     return mapping
