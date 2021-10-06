25986cc47b51741233f931cb961be134063fd931
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-06 15:20:28 +0200

    minor fix

--- CandidateGeneration/SourceTargetExpander/expand_sources.py
+++ CandidateGeneration/SourceTargetExpander/expand_sources.py
@@ -34,7 +34,6 @@ def expandSources(json_object, sources):
     expanded_gender = expandGender(sources['p_gender'])
     expanded_age = expandAge(sources['p_age'])
     expanded_condition = expandCondition(sources['p_condition'])
-    print( expanded_condition )
 
     expanded_sources['ep_gender'] = expanded_gender
     expanded_sources['ep_age'] = expanded_age
