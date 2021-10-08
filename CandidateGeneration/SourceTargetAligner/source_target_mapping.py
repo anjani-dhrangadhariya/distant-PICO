174021d4b1f76dee7e1cefe5e2ab018fee62f467
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-08 15:45:56 +0200

    added a mapping

--- CandidateGeneration/SourceTargetAligner/source_target_mapping.py
+++ CandidateGeneration/SourceTargetAligner/source_target_mapping.py
@@ -9,6 +9,7 @@ def generateMapping():
     mapping['ep_age'] =  ['BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription']
     mapping['ep_condition'] =  ['BriefSummary', 'DetailedDescription']
     mapping['ei_name'] =  ['BriefSummary', 'DetailedDescription']
+    mapping['ei_syn'] =  ['BriefSummary', 'DetailedDescription']
     mapping['es_type'] =  ['BriefSummary', 'DetailedDescription']
 
     return mapping
