414eeaec2b59cc7d757a2804492e5096bcd5a893
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-11 14:29:21 +0200

    minor improvement

--- CandidateGeneration/SourceTargetAligner/labeling_operators.py
+++ CandidateGeneration/SourceTargetAligner/labeling_operators.py
@@ -14,18 +14,44 @@ def directAligner(source, targets, candidateTargets):
 
     for eachSource in source: # each source_i
 
-        for eachCandidate in candidateTargets: # each target_i
+        for key, value in targets.items(): # each target_i
 
-            target_i = targets[eachCandidate]['text']
-            annotations = align_highconf_longtarget( target_i.lower() , eachSource.lower() )
+            res = key.startswith(tuple(candidateTargets))
+            if res == True:
+                target_i = targets[key]['text']
+                annotations = align_highconf_longtarget( target_i.lower() , eachSource.lower() )
 
-            if annotations:
-                if eachCandidate not in combined_annotations:
-                    combined_annotations[eachCandidate] = [annotations]
-                else:
-                    combined_annotations[eachCandidate].append( annotations )
+                if annotations:
+                    if key not in combined_annotations:
+                        combined_annotations[key] = [annotations]
+                    else:
+                        combined_annotations[key].append( annotations )
 
     return combined_annotations
+
+def longTailInterventionAligner(source, targets, candidateTargets):
+
+    intervention_annotations = dict()
+
+    for eachKey, eachValue in source.items(): # each source_i
+
+        intervention_term = list(eachValue)[0]['text']
+
+        for key, value in targets.items(): # each target_i
+
+            res = key.startswith(tuple(candidateTargets))
+            if res == True:
+                target_i = targets[key]['text']
+
+                annotations = align_highconf_longtarget( target_i.lower() , intervention_term.lower() )
+
+                if annotations:
+                    if key not in intervention_annotations:
+                        intervention_annotations[key] = [annotations]
+                    else:
+                        intervention_annotations[key].append( annotations )
+
+    return intervention_annotations
 
 def longTailAligner(source, targets, candidateTargets):
 
