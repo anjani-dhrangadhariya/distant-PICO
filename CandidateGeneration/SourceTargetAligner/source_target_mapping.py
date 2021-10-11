afc857fa106452ea54cb473e2e48529de9d2da3d
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-11 15:37:53 +0200

    improved mapping

--- CandidateGeneration/SourceTargetAligner/source_target_mapping.py
+++ CandidateGeneration/SourceTargetAligner/source_target_mapping.py
@@ -4,16 +4,19 @@ def generateMapping():
 
     mapping = dict()
 
-    mapping['ep_sample_size'] =  ['BriefSummary', 'DetailedDescription']
-    mapping['ep_gender'] =  ['EligibilityCriteria', 'BriefSummary', 'DetailedDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
-    mapping['ep_age'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription']
     mapping['ep_condition'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription']
+    mapping['ep_age'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription']
+    mapping['ep_gender'] =  ['EligibilityCriteria', 'BriefSummary', 'DetailedDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
+    mapping['ep_sample_size'] =  ['BriefSummary', 'DetailedDescription']
+
 
     mapping['ei_name'] =  ['BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription']
     mapping['ei_syn'] =  ['BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription']
 
-    mapping['eo_primary'] =  ['PriOutcomeDesc', 'SecOutcomeDesc']
-    mapping['eo_secondary'] =  ['PriOutcomeDesc', 'SecOutcomeDesc']
+
+    mapping['eo_primary'] =  ['PriOutcomeDesc', 'SecOutcomeDesc', 'BriefSummary', 'DetailedDescription']
+    mapping['eo_secondary'] =  ['PriOutcomeDesc', 'SecOutcomeDesc', 'BriefSummary', 'DetailedDescription']
+
 
     mapping['es_type'] =  ['InterventionDescription', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'DesignInterventionModelDescription']
 
