cbcf8370de579bf325903c96f24014fde9ff12ef
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-12 10:40:28 +0200

    improved data structure for targets

--- CandidateGeneration/SourceTargetExpander/expand_targets.py
+++ CandidateGeneration/SourceTargetExpander/expand_targets.py
@@ -8,12 +8,17 @@ print( ExpandTargetsDoc.__doc__ )
 
 
 # imports - general
+import enum
 import time
 
 # imports - linguistic
 import re
 import spacy
 from scispacy.abbreviation import AbbreviationDetector
+from spacy.lang.en import English
+
+nlp = English()
+nlp.add_pipe("sentencizer")
 
 # 
 from SourceTargetExpander.SourceExpander.expansion_utils import *
@@ -35,11 +40,28 @@ def expandTargets(json_object, targets):
     expanded_targets = dict()
 
     for key, value in targets.items():
+
         newline_removed = value.replace("\n", " ").replace("\r", " ")
         trailingspaces_removed = removeSpaceTrailsString(newline_removed)
 
-        # Get POS-tags for the target string
-        possed_targets = getPOStags(trailingspaces_removed)
-        expanded_targets[key] = possed_targets
+        sep = 'Exclusion Criteria'
+        if key == 'EligibilityCriteria':
+            if sep in trailingspaces_removed:
+                trailingspaces_removed = trailingspaces_removed.split(sep, 1)[0]
+
+        # Convert long targets to sentences
+        target_sentences = nlp(trailingspaces_removed)
+
+        sent_parsed = dict()
+        
+        for ith, eachSent in enumerate(target_sentences.sents):
+
+            # Get POS-tags for the target sub-string
+            possed_targets = getPOStags( str(eachSent) )
+            nested_key = 'sentence_' + str(ith)
+            sent_parsed[nested_key] = possed_targets
+
+        if key not in expanded_targets:
+            expanded_targets[key] = sent_parsed
 
     return expanded_targets
