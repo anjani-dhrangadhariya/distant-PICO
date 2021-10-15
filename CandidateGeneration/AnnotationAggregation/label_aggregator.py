fa72cc9d9841d527e1e06998ca6eceb93783f717
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-15 18:42:28 +0200

    minor improvement in aggregator

--- CandidateGeneration/AnnotationAggregation/label_aggregator.py
+++ CandidateGeneration/AnnotationAggregation/label_aggregator.py
@@ -1,7 +1,16 @@
 
 def aggregate_labels(to_aggregate, aggregation_collector):
 
-    # print( aggregation_collector ) 
+    print( to_aggregate )
 
-    for key, value in to_aggregate.items():
-        print( key )  
+    for key, annotation_main in to_aggregate.items():
+        
+        for a_key, a_value in annotation_main.items():
+
+            if 'source' not in a_key:
+                if a_key in aggregation_collector:
+                    print('########################################################')
+                    aggregation_collector[a_key].append( a_value[0] )
+                    print( aggregation_collector[a_key] )
+
+        # print( aggregation_collector )
