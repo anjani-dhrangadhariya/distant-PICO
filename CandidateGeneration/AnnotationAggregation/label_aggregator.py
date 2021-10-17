1577b899bbd216ef277fdaba6a004d141b1d742e
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-17 15:34:46 +0200

    aggregator at the individual entity level

--- CandidateGeneration/AnnotationAggregation/label_aggregator.py
+++ CandidateGeneration/AnnotationAggregation/label_aggregator.py
@@ -1,11 +1,22 @@
 
+'''
+Description:
+    The function directly aligns a list of "Participant: Condition" terms from the source list to the appropriate targets
+
+Args:
+    source (list): list to candidate participant condition terms to be aligned with the targets
+        targets (dict): A dictionary with all the targets for distant-PICOS
+        candidateTargets (dict): A dictionary to select appropriate targets for each of the PICOS sources
+        PICOS (int): Label for the entity being weakly annotations
+
+Returns:
+    dictionary: weakly annotated sources with the givens sources and PICOS labeling scheme
+'''
 def aggregate_labels(to_aggregate, aggregation_collector):
 
     intra_aggregator = dict()
 
     # print( 'before aggregation: ',  to_aggregate )
-
-    # print( aggregation_collector )
 
     for key, value in to_aggregate.items():
         for a_key, a_value in value.items():
@@ -27,7 +38,36 @@ def aggregate_labels(to_aggregate, aggregation_collector):
                             intra_aggregator[a_key][b_key] = b_value
 
 
-    print( 'Intra aggregator: ', intra_aggregator )
+    # print( 'Intra aggregator: ', intra_aggregator )
 
-    print('############################## END of NCT ID ######################################')
-    # Intra aggregation
+    # XXX Aggregate intra-aggregator with the inter-aggragator
+    # Add the intra-aggregator content into inter-aggragator
+
+    isEmpty = bool(aggregation_collector)
+    isIntra_aggregatorEmpty = bool(intra_aggregator)
+    if isEmpty == False: # If aggregation_collector is empty
+        aggregation_collector = intra_aggregator
+    else:
+        # if intra_aggregator is not empty Aggregate intra_aggregator to the aggregation_collector....
+        if isIntra_aggregatorEmpty == True:          
+
+            for key, value in intra_aggregator.items():
+                if key not in aggregation_collector:
+                    aggregation_collector[key] = value
+                else:
+                    # print(key)
+                    for a_key, a_value in value.items():
+                        
+                        if a_key in aggregation_collector[key]:
+                            # print( '-----', a_key )
+                            aggregate_this = a_value[1]
+                            to_this = aggregation_collector[key][a_key][1]
+
+                            for count, (i, j)  in enumerate(zip(aggregate_this, to_this)):
+                                if i > j:
+                                    aggregation_collector[key][a_key][1][count] = i
+                        else:
+                            aggregation_collector[key][a_key] = a_value
+
+
+    return aggregation_collector
