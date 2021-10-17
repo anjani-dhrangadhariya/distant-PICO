e1920eaf5440b4a7e0c88b22f6f535d54ea17715
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-17 13:09:36 +0200

    intra aggregation developed

--- CandidateGeneration/AnnotationAggregation/label_aggregator.py
+++ CandidateGeneration/AnnotationAggregation/label_aggregator.py
@@ -1,16 +1,33 @@
 
 def aggregate_labels(to_aggregate, aggregation_collector):
 
-    print( to_aggregate )
+    intra_aggregator = dict()
 
-    for key, annotation_main in to_aggregate.items():
-        
-        for a_key, a_value in annotation_main.items():
+    # print( 'before aggregation: ',  to_aggregate )
 
-            if 'source' not in a_key:
-                if a_key in aggregation_collector:
-                    print('########################################################')
-                    aggregation_collector[a_key].append( a_value[0] )
-                    print( aggregation_collector[a_key] )
+    # print( aggregation_collector )
 
-        # print( aggregation_collector )
+    for key, value in to_aggregate.items():
+        for a_key, a_value in value.items():
+            if a_key != 'source':
+                if a_key not in intra_aggregator:
+                    intra_aggregator[a_key] = a_value
+                else:
+                    # Get the key-value and aggregate at the level of value
+                    for b_key, b_value in a_value.items():
+                        if b_key in intra_aggregator[a_key]: # if the sentence IDs are identical aggregate at the sentence level
+
+                            aggregate_this = b_value[1]
+                            to_this = intra_aggregator[a_key][b_key][1]
+
+                            for count, (i, j)  in enumerate(zip(aggregate_this, to_this)):
+                                if i > j:
+                                    intra_aggregator[a_key][b_key][1][count] = i
+                        else: # if the sentence IDs are different and not yet in the intra-aggregation dictionary
+                            intra_aggregator[a_key][b_key] = b_value
+
+
+    print( 'Intra aggregator: ', intra_aggregator )
+
+    print('############################## END of NCT ID ######################################')
+    # Intra aggregation
