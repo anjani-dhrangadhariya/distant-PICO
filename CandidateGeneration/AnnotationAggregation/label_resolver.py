45c270e7c4e237bdbc03ed0b524ce4b8bb11f942
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-25 12:10:58 +0200

    functions added

--- CandidateGeneration/AnnotationAggregation/label_resolver.py
+++ CandidateGeneration/AnnotationAggregation/label_resolver.py
@@ -1,6 +1,23 @@
-def resolveLabels():
+import collections
+import re
+import nltk
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+from joblib import dump, load
 
-    # Resolves the overlapping labels 
-    
+def my_tokenizer(doc):
+    tokens = doc.split(' ')
+    return tokens
 
-    return None
+
+def text_prepare(text):
+    REPLACE_BY_SPACE_RE = re.compile('[!@#$%^&*()[]{};:,./<>?\|`~=_+]')
+    STOPWORDS = set(stopwords.words('english'))
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+    text = REPLACE_BY_SPACE_RE.sub(' ', str(text))# replace REPLACE_BY_SPACE_RE symbols by space in text
+    text = ' '.join([w for w in text.split() if not w in STOPWORDS])# delete stopwords from text
+    return text.lower()
