be9429e0c8e0c3af7912799dc8339a9a15a84b8d
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-25 21:38:17 +0200

    label resolver added

--- CandidateGeneration/AnnotationAggregation/label_aggregator.py
+++ CandidateGeneration/AnnotationAggregation/label_aggregator.py
@@ -1,5 +1,11 @@
 
 import collections
+import enum
+import re
+import nltk
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+from joblib import dump, load
 from sanity_checks import *
 from AnnotationAggregation.label_resolver import *
 
@@ -158,10 +164,71 @@ Args:
 Returns:
     dictionary: weakly annotated sources with the givens sources and PICOS labeling scheme
 '''
-def merge_labels(p, ic, o, s, inter_aggregator):
+def merge_labels(globally_aggregated):
 
-    # Iterate through the labels here and whenever there is an overlap, resolve using the label resolver.
-    
+    globally_merged = dict()
 
+    # Load the resolver model
+    label_resolver = load('/home/anjani/distant-PICO/CandidateGeneration/AnnotationAggregation/resolver_models/resolver.joblib') 
 
-    return None
+    # Iterate through the labels here
+    for key, target in globally_aggregated.items():
+
+        if key not in globally_merged:
+            globally_merged[key] = {}
+
+        if 'id' not in key: # do not iterate the NCT_ID
+            for a_key, sentence in target.items():
+
+                # Merge and collect the annotations here
+                annotations = [0] * len(sentence['tokens'])
+
+                if a_key not in globally_merged[key]:
+                    globally_merged[key][a_key] = {}
+                    globally_merged[key][a_key]['tokens'] = sentence['tokens']
+
+                if len(sentence) == 2: # No resolution needed for this case
+                    annot_index = list(sentence.keys())[-1]
+                    annotations = sentence[annot_index]
+                    globally_merged[key][a_key]['annotation'] = annotations
+
+                
+                elif len(sentence) >= 3: # resolution needed for this case as there could be some overlap
+
+                    resultset = [ value for key, value in sentence.items() if key not in ['tokens']]
+
+                    phrase = []
+                    counter_values = []
+                    overlap = []
+
+                    for counter, i in enumerate(zip(*resultset))  :
+                        if sum([1 if n else 0 for n in i])  > 1:
+                            phrase.append( globally_merged[key][a_key]['tokens'][counter] )
+                            counter_values.append( counter )
+                            if 0 in i:
+                                temp_i = list(i)
+                                temp_i.sort()
+                                temp_i =  temp_i[1:]
+                                overlap.append( tuple( temp_i ) )
+                            else:
+                                overlap.append( i )
+                        elif sum([1 if n else 0 for n in i])  == 1:
+                            temp_i = list(i)
+                            temp_i.sort()
+                            annotations[counter] = temp_i[-1]
+
+                    if phrase:
+                        label_resolved = label_resolver.predict( phrase )
+                        label_resolved_proba = label_resolver.predict_proba( phrase )
+
+                        for word, over, counter, l, l_p in zip(phrase, overlap, counter_values, label_resolved, label_resolved_proba):
+                            if l in over:
+                                annotations[counter] = l
+                            else:
+                                print( word, over, counter, l, l_p )
+                                temp_i = list(over)
+                                chosen_index = [ l_p[ n-1 ] for n in temp_i]
+                                chosen_annot = max( chosen_index )
+                                annotations[counter] = list(l_p).index(chosen_annot) + 1
+
+    return globally_merged
