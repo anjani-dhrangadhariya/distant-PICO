f743d2d127e7944ffba09440f6cf918fa6b34848
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-31 16:56:44 +0100

    minor changes

--- ModelTraining/read_candidates.py
+++ ModelTraining/read_candidates.py
@@ -13,12 +13,14 @@ import time
 
 import numpy as np
 import pandas as pd
+from memory_profiler import profile
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
 from VectorBuilders.contextual_vector_builder import *
+from Utilities.experiment_arguments import *
 
-
+# @profile
 def readRawCandidates( list_NCT, label_type=None ):
 
     nct_ids = []
@@ -63,20 +65,22 @@ def readRawCandidates( list_NCT, label_type=None ):
 
     return df
 
-    return None
-
 
 def fetchAndTransformCandidates():
 
-    list_NCT = '/mnt/nas2/data/systematicReview/clinical_trials_gov/Weak_PICO/PICOS_data_preprocessed/merged_1_0.txt'
-
-    MAX_LEN = 100
+    args = getArguments() # get all the experimental arguments
 
     start_candidate_reading = time.time()
-    raw_candidates = readRawCandidates( list_NCT, label_type=None )
+    raw_candidates = readRawCandidates( args.input_file, label_type=None )
     print("--- Took %s seconds to read the raw weakly annotated candidates ---" % (time.time() - start_candidate_reading))
 
     start_candidate_transformation = time.time()
-    input_embeddings, input_labels, input_masks, input_pos = getContextualVectors( raw_candidates, 'bert', MAX_LEN )
+    input_embeddings, input_labels, input_masks, input_pos, tokenizer = getContextualVectors( raw_candidates, args.embed, args.max_len )
     assert len( input_embeddings ) == len( raw_candidates )
     print("--- Took %s seconds to transform the raw weakly annotated candidates ---" % (time.time() - start_candidate_transformation))
+
+    candidates_df = raw_candidates.assign(embeddings = pd.Series(input_embeddings).values, label_pads = pd.Series(input_labels).values, attn_masks = pd.Series(input_masks).values, inputpos = pd.Series(input_pos).values) # assign the padded embeddings to the dataframe
+
+    del input_embeddings, input_labels, input_masks, input_pos     # Delete the large variables
+
+    return candidates_df, args, tokenizer
