77509661c41da653ff41b5a23c2edc6de4b99048
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-31 13:28:25 +0100

    minor assertion tests added

--- ModelTraining/read_candidates.py
+++ ModelTraining/read_candidates.py
@@ -16,12 +16,15 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
+from VectorBuilders.contextual_vector_builder import *
+
 
 def readRawCandidates( list_NCT, label_type=None ):
 
     nct_ids = []
     tokens = []
     labels = []
+    pos = []
 
     with open(list_NCT, 'r', encoding='latin1') as NCT_ids_file:
 
@@ -38,13 +41,18 @@ def readRawCandidates( list_NCT, label_type=None ):
                         labels.append( sentence['annotation'] )
                         nct_ids.append( id_ )
 
+                        # Generate dummy POS items
+                        pos_i = [0] * len( sentence['tokens'] )
+                        pos.append( pos_i )
+
             if i == 2:
                 break
 
     corpus_df = pd.DataFrame(
         {'ids': nct_ids,
         'tokens': tokens,
-        'labels': labels
+        'labels': labels,
+        'pos': pos
         })
 
     df = corpus_df.sample(frac=1).reset_index(drop=True) # Shuffles the dataframe after creation
@@ -60,12 +68,15 @@ def readRawCandidates( list_NCT, label_type=None ):
 
 def fetchAndTransformCandidates():
 
-    start_candidate_reading = time.time()
-
     list_NCT = '/mnt/nas2/data/systematicReview/clinical_trials_gov/Weak_PICO/PICOS_data_preprocessed/merged_1_0.txt'
 
-    raw_candidates = readRawCandidates( list_NCT, label_type=None )
+    MAX_LEN = 100
 
+    start_candidate_reading = time.time()
+    raw_candidates = readRawCandidates( list_NCT, label_type=None )
     print("--- Took %s seconds to read the raw weakly annotated candidates ---" % (time.time() - start_candidate_reading))
 
-
+    start_candidate_transformation = time.time()
+    input_embeddings, input_labels, input_masks, input_pos = getContextualVectors( raw_candidates, 'bert', MAX_LEN )
+    assert len( input_embeddings ) == len( raw_candidates )
+    print("--- Took %s seconds to transform the raw weakly annotated candidates ---" % (time.time() - start_candidate_transformation))
