801174300b6042c5bf7b235ca1e9f17a597d3297
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-10-31 18:02:47 +0100

    stable train function

--- ModelTraining/train_eval_predict.py
+++ ModelTraining/train_eval_predict.py
@@ -37,6 +37,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 # sklearn
+import sklearn
 from sklearn import preprocessing
 from sklearn.metrics import (accuracy_score, classification_report,
                              confusion_matrix, f1_score, plot_confusion_matrix,
@@ -58,6 +59,12 @@ from transformers import (AdamW, AutoModel, AutoModelForTokenClassification,
                           GPT2Config, GPT2Model, GPT2Tokenizer, RobertaConfig,
                           RobertaModel, get_linear_schedule_with_warmup)
 
+import warnings
+warnings.filterwarnings('ignore')
+
+def printMetrics(cr):
+    
+    return cr['macro avg']['f1-score'], cr['1']['f1-score'], cr['2']['f1-score'], cr['3']['f1-score'], cr['4']['f1-score']
 
 # Train
 def train(defModel, optimizer, scheduler, train_dataloader, exp_args, eachSeed):
@@ -67,6 +74,10 @@ def train(defModel, optimizer, scheduler, train_dataloader, exp_args, eachSeed):
         for epoch_i in range(0, exp_args.max_eps):
             # Accumulate loss over an epoch
             total_train_loss = 0
+
+            # (coarse-grained) accumulate predictions and labels over the epoch
+            train_epoch_logits_coarse_i = []
+            train_epochs_labels_coarse_i = []
 
             # Training for all the batches in this epoch
             for step, batch in enumerate(train_dataloader):
@@ -79,8 +90,43 @@ def train(defModel, optimizer, scheduler, train_dataloader, exp_args, eachSeed):
                 b_masks = batch[2].to(f'cuda:{defModel.device_ids[0]}')
                 b_pos = batch[3].to(f'cuda:{defModel.device_ids[0]}')
 
-                b_loss, b_output, b_labels, b_mask = defModel(input_ids = b_input_ids, attention_mask=b_masks, labels=b_labels, input_pos=b_pos)                
+                b_loss, b_output, b_labels, b_mask = defModel(input_ids = b_input_ids, attention_mask=b_masks, labels=b_labels, input_pos=b_pos)
+
+                total_train_loss += abs( torch.mean(b_loss) ) 
+
+                abs( torch.mean(b_loss) ).backward()
+
+                # Clip the norm of the gradients to 1.0. This is to help prevent the "exploding gradients" problem.
+                torch.nn.utils.clip_grad_norm_(defModel.parameters(), 1.0)
+
+                #Optimization step
+                optimizer.step()
+
+                # Update the learning rate.
+                scheduler.step()
+
+                for i in range(0, b_labels.shape[0]):
+
+                    selected_preds_coarse = torch.masked_select( b_output[i, ].to(f'cuda:{defModel.device_ids[0]}'), b_mask[i, ])
+                    selected_labs_coarse = torch.masked_select(b_labels[i, ].to(f'cuda:{defModel.device_ids[0]}'), b_mask[i, ])
+
+                    train_epoch_logits_coarse_i.extend( selected_preds_coarse.to("cpu").numpy() )
+                    train_epochs_labels_coarse_i.extend( selected_labs_coarse.to("cpu").numpy() )
 
 
+                if step % exp_args.print_every == 0:
 
+                    cr = sklearn.metrics.classification_report(y_pred= train_epoch_logits_coarse_i, y_true= train_epochs_labels_coarse_i, labels= list(range(5)), output_dict=True)
+                    f1, f1_1 , f1_2, f1_3, f1_4 = printMetrics(cr)
+                    print('Training: Epoch {} with macro average F1: {}, F1 score (P): {}, F1 score (IC): {}, F1 score (O), F1 score (S): {}'.format(epoch_i, f1, f1_1 , f1_2, f1_3, f1_4))
 
+            # Calculate the average loss over all of the batches.
+            avg_train_loss = total_train_loss / len(train_dataloader)
+
+            train_cr = classification_report(y_pred= train_epoch_logits_coarse_i, y_true=train_epochs_labels_coarse_i, labels= list(range(5)), output_dict=True)             
+
+            # Delete the collected logits and labels
+            del train_epoch_logits_coarse_i, train_epochs_labels_coarse_i
+            gc.collect()
+
+            
