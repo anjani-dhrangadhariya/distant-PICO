1135278ed61c6b214de0ee1f37f2ca8e2280e5e0
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-11-10 16:39:46 +0100

    improved study type regex

--- CandidateGeneration/SourceTargetExpander/SourceExpander/expand_stdtype.py
+++ CandidateGeneration/SourceTargetExpander/SourceExpander/expand_stdtype.py
@@ -24,11 +24,11 @@ def expandStudyType(studytype_source):
 
     ''' Expanded according the MeSH entry term (MeSH ID: D011897) from U.S. National Library of Medicine (NLM)'''
     # randomized_source = ['Random', 'Randomized', 'Randomised', 'Randomization', 'Randomisation', 'Randomly']
-    randomized_source_pattern = '([rR]andom(i[sz]ed|ly|i[sz]ation)?)' # only group 1 will be considered
+    randomized_source_pattern = '(([rR]andom(i[sz]ed|ly|i[sz]ation)?)+( controlled)?( trials?)?)' # only group 1 will be considered
 
     ''' Expanded according the MeSH entry term (MeSH ID: D065228) from U.S. National Library of Medicine (NLM)'''
     # nonrandomized_source = ['Non-Random', 'Nonrandom', 'Non Random', 'Non-Randomized', 'Non-Randomised', 'Nonrandomized', 'Nonrandomised', 'Non Randomized', 'Non Randomised']
-    nonrandomized_source_pattern = '([nN]o[nt][- ]?[rR]andom(i[sz]ed|ly|i[sz]ation)?)' # only group 1 will be considered
+    nonrandomized_source_pattern = '(([rR]andom(i[sz]ed|ly|i[sz]ation)?)+(,? controlled)?( trials?)?)' # only group 1 will be considered
 
     if studytype_source == 'N/A':
         return 'N.A.'
