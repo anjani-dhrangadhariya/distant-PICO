16c64646914352e583ebfa4f4a3d3b5acfe53f4d
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-11-16 17:02:38 +0100

    combined primary and secondary outcomes

--- CandidateGeneration/SourceFetcher/outcome_sourcefetcher.py
+++ CandidateGeneration/SourceFetcher/outcome_sourcefetcher.py
@@ -72,9 +72,11 @@ def fetchOutcomeSources(json_document):
     combined_sources = dict()
 
     o_primary = fetchPrimaryOutcome(json_document)
-    combined_sources['o_primary'] = o_primary
+    # combined_sources['o_primary'] = o_primary
 
     o_secondary = fetchSecondaryOutcome(json_document)
-    combined_sources['o_secondary'] = o_secondary
+    # combined_sources['o_secondary'] = o_secondary
+
+    combined_sources['o_name'] = {**o_primary, **o_secondary}
 
     return combined_sources
