0e8e135e5c7483fd702462c18f91111d13c05b52
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-11-18 18:28:15 +0100

    function to reorganize sources

--- CandidateGeneration/SourceTargetExpander/expand_sources.py
+++ CandidateGeneration/SourceTargetExpander/expand_sources.py
@@ -50,8 +50,9 @@ def expandSources(json_object, sources):
     # -------------------------------------------------------------------------------------
     # I/C Interventions Comparators 
     # -------------------------------------------------------------------------------------
-    expanded_intervention = expandIntervention(sources['i_name'], fetch_pos=True, fetch_abb=True)
-    expanded_sources['ei_name'] = expanded_intervention
+    if 'i_name' in sources:
+        expanded_intervention = expandIntervention(sources['i_name'], fetch_pos=True, fetch_abb=True)
+        expanded_sources['ei_name'] = expanded_intervention
 
     # -------------------------------------------------------------------------------------
     # Outcomes
@@ -59,7 +60,6 @@ def expandSources(json_object, sources):
     if 'o_name' in sources:
         expanded_prim_outcomes = expandOutcomes(sources['o_name'])
         expanded_sources['eo_name'] = expanded_prim_outcomes
-        print( expanded_prim_outcomes )
 
     # -------------------------------------------------------------------------------------
     # Study type
@@ -69,3 +69,49 @@ def expandSources(json_object, sources):
         expanded_sources['es_type'] = expanded_studytype
 
     return expanded_sources
+
+
+def expandSources(json_object, sources):
+
+    organized_sources = dict()
+
+
+    # -------------------------------------------------------------------------------------
+    # Participant subclasses 
+    # -------------------------------------------------------------------------------------
+    if 'p_sample_size' in sources:
+        organized_sources['p_sample_size'] = sources['p_sample_size']
+
+    if 'p_gender' in sources and 'All' not in sources['p_gender']:
+        organized_sources['p_sample_size'] = [sources['p_gender']]
+    elif 'p_gender' in sources and 'All' in sources['p_gender']:
+        organized_sources['p_sample_size'] = ['Male', 'Female']
+
+    if 'p_age' in sources and 'StdAge' in sources['p_age']:
+        organized_sources['p_age'] = sources['p_age']['StdAge']
+
+    if 'p_condition' in sources:
+        organized_sources['p_condition'] = sources['p_condition']
+
+        
+    # -------------------------------------------------------------------------------------
+    # I/C Interventions Comparators 
+    # -------------------------------------------------------------------------------------
+    if 'i_name' in sources:
+        int_list = [ v for k,v in sources['i_name'].items() if k.startswith('name_') ]
+        organized_sources['i_name'] = int_list
+
+    # -------------------------------------------------------------------------------------
+    # Outcomes
+    # -------------------------------------------------------------------------------------
+    if 'o_name' in sources:
+        out_list = [ v for k,v in sources['o_name'].items() ]
+        organized_sources['o_name'] = out_list
+    
+    # -------------------------------------------------------------------------------------
+    # Study type
+    # -------------------------------------------------------------------------------------
+    if 's_type' in sources and 'N/A' not in sources['s_type']:
+        organized_sources['es_type'] = sources['s_type']
+
+    return organized_sources
