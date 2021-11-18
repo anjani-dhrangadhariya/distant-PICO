6fd996e135f44af5cdcec907e1ae2ad4240253f6
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-11-18 18:27:26 +0100

    function transfers

--- CandidateGeneration/CandGenUtilities/labeler_utilities.py
+++ CandidateGeneration/CandGenUtilities/labeler_utilities.py
@@ -1,2 +1,18 @@
 def allowedPOS():
     return list(['NOUN', 'PROPN', 'VERB', 'ADJ'])
+
+def generateLabels():
+
+    PICOS = dict()
+
+    PICOS['p'] = 1
+    PICOS['i'] = 2
+    PICOS['o'] = 3
+    PICOS['s'] = 4
+    PICOS['ABSTAIN'] = -1
+    PICOS['Ospan'] = 0
+    
+    return PICOS
+
+def generateAntiLabels(PICOS):
+    return {v: k for k, v in PICOS.items()}
