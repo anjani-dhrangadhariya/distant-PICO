511bcc5658bc8ac124f92c277281b57f5fb595b0
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-11-18 15:20:42 +0100

    updated mapping

--- CandidateGeneration/SourceTargetAligner/source_target_mapping.py
+++ CandidateGeneration/SourceTargetAligner/source_target_mapping.py
@@ -13,21 +13,16 @@ def generateMapping():
 
     mapping = dict()
 
-    mapping['ep_condition'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription']
-    mapping['ep_age'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription']
-    mapping['ep_gender'] =  ['EligibilityCriteria', 'BriefSummary', 'DetailedDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
-    mapping['ep_sample_size'] =  ['BriefSummary', 'DetailedDescription']
+    mapping['ep_condition'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
+    mapping['ep_age'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
+    mapping['ep_gender'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
+    mapping['ep_sample_size'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
 
+    mapping['ei_name'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
 
-    mapping['ei_name'] =  ['BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription']
-    mapping['ei_syn'] =  ['BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription']
+    mapping['eo_name'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription', 'PriOutcomeDesc', 'SecOutcomeDesc']
 
-
-    mapping['eo_name'] =  ['PriOutcomeDesc', 'SecOutcomeDesc', 'BriefSummary', 'DetailedDescription', 'BriefTitle', 'OfficialTitle']
-    # mapping['eo_secondary'] =  ['PriOutcomeDesc', 'SecOutcomeDesc', 'BriefSummary', 'DetailedDescription', 'BriefTitle', 'OfficialTitle']
-
-
-    mapping['es_type'] =  ['InterventionDescription', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'DesignInterventionModelDescription']
+    mapping['es_type'] =  ['EligibilityCriteria', 'BriefTitle', 'OfficialTitle', 'BriefSummary', 'DetailedDescription', 'InterventionDescription', 'PriOutcomeDesc', 'SecOutcomeDesc', 'DesignInterventionModelDescription']
 
     return mapping
 
