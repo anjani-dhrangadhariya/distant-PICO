dde6c94087a2b3c02b7599b1457a8c942f010f10
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2021-12-27 17:17:48 +0100

    separate function for mysql database connection

--- CandidateGeneration/Ontologies/parseOntlogies.py
+++ CandidateGeneration/Ontologies/parseOntlogies.py
@@ -16,9 +16,24 @@ from sqlite3 import Error
 import msgpack
 import pandas as pd
 
+def createMySQLConn(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+@staticmethod
 def init_sqlite_tables(fpath, dataframe):
 
-    conn = sqlite3.connect(fpath)
+    conn = createMySQLConn(fpath)
     sql = """CREATE TABLE IF NOT EXISTS terminology1 (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     SAB text NOT NULL,
@@ -44,7 +59,7 @@ def init_sqlite_tables(fpath, dataframe):
     conn.commit()
     conn.close()
 
-
+@staticmethod
 def cui2tuiMapper(indir, outdir, tui2pio):
 
     # validate that the UMLS source REFs are provided
