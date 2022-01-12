b4248f982e1a438680fb9e7b3211dfb6c4092e4e
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-12 18:26:52 +0100

    improving

--- CandidateGeneration/LabelingFunctions/ontologyLF.py
+++ CandidateGeneration/LabelingFunctions/ontologyLF.py
@@ -1,5 +1,5 @@
 from nltk import ngrams
-
+import re
 
 def OntologyLabelingFunction(text, term, max_ngram: int = 5, abstain_decision: bool = True, case_sensitive: bool = False):
 
@@ -7,12 +7,17 @@ def OntologyLabelingFunction(text, term, max_ngram: int = 5, abstain_decision: b
         onto_term = t[0]
 
         if len( onto_term.split() ) > max_ngram:
-            fivegrams = ngrams(onto_term, max_ngram)
-            print( list(fivegrams) )
+            fivegrams = ngrams(onto_term.split(), max_ngram)
+            # print( list(fivegrams) )
 
         for t_i in [onto_term, onto_term.lower(), onto_term.rstrip('s'), onto_term + 's']:
             if t_i in text:
-                pass
-            # else:
-            #     if len( t_i.split(' ') ) > 5:
-            #         print( t_i )
+
+                # Use matcher to get exact match and spans
+                r = re.compile(t_i)
+                spans = [[m.start(),m.end()] for m in r.finditer(text)]
+                print( spans )
+
+            
+        if i == 10:
+            break
