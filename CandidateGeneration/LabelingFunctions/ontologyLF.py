c46dbeb69886e1c89a2fe723167ee5d6164deb42
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-12 17:55:10 +0100

    code for max_ngrams added

--- CandidateGeneration/LabelingFunctions/ontologyLF.py
+++ CandidateGeneration/LabelingFunctions/ontologyLF.py
@@ -1,10 +1,18 @@
-def OntologyLabelingFunction(text, term, abstain_decision: bool = True, case_sensitive: bool = False):
+from nltk import ngrams
+
+
+def OntologyLabelingFunction(text, term, max_ngram: int = 5, abstain_decision: bool = True, case_sensitive: bool = False):
 
     for i, t in enumerate(term):
         onto_term = t[0]
+
+        if len( onto_term.split() ) > max_ngram:
+            fivegrams = ngrams(onto_term, max_ngram)
+            print( list(fivegrams) )
+
         for t_i in [onto_term, onto_term.lower(), onto_term.rstrip('s'), onto_term + 's']:
             if t_i in text:
                 pass
-            else:
-                if len( t_i.split(' ') ) > 5:
-                    print( t_i )
+            # else:
+            #     if len( t_i.split(' ') ) > 5:
+            #         print( t_i )
