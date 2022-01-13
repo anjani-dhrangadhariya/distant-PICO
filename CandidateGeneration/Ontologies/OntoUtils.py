9cc44f498ee7a8d70814ba613fee97e0230b9267
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-13 14:40:24 +0100

    ranking, partition added

--- CandidateGeneration/Ontologies/OntoUtils.py
+++ CandidateGeneration/Ontologies/OntoUtils.py
@@ -67,12 +67,38 @@ def termCountThreshold(umls_d):
 
 def rankSAB(umls_d):
 
-    keys =  list(umls_d.keys())
-    random.shuffle(keys)
-    umls_d = [(key, umls_d[key]) for key in keys]
+    keys = [k for k in umls_d.keys()] 
 
-    return umls_d
+    shuffle(keys)
+
+    umls_d_new = dict()
+
+    for k in keys:
+        umls_d_new[k] = umls_d[k]
+
+    return umls_d_new
+
+def list2Nested(l, nested_length):
+    return [l[i:i+nested_length] for i in range(0, len(l), nested_length)]
 
 def partitionRankedSAB(umls_d):
 
-    return None
+    keys = list(umls_d.keys())
+
+    partitioned_lfs = [ ]
+
+    for i in range( 0, len(keys) ):
+
+        if i == 0 or i == len(keys):
+            if i == 0:
+                partitioned_lfs.append( keys )
+            if i ==len(keys):
+                temp3 = list2Nested(keys, 1)
+                partitioned_lfs.append( temp3 )
+        else:
+            temp1, temp2 = keys[:i] , keys[i:]
+            temp3 = list2Nested( keys[:i], 1)
+            temp3.append( keys[i:] )
+            partitioned_lfs.append( temp3 )
+
+    return partitioned_lfs
