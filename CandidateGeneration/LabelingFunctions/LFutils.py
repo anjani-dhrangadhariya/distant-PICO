c0423de191df02918b68b9c012c912707059628a
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-13 18:09:20 +0100

    common utilities for LFs

---
+++ CandidateGeneration/LabelingFunctions/LFutils.py
@@ -0,0 +1,29 @@
+from nltk.tokenize import WhitespaceTokenizer, sent_tokenize, word_tokenize
+from nltk import ngrams
+
+
+def getNgrams():
+
+
+    return None
+
+def expandTerm( term , max_ngram, fuzzy_match):
+    
+    expandedTerm = []
+    termVariations = []
+
+    if len( term.split() ) > max_ngram:
+        fivegrams = ngrams(term.split(), max_ngram)
+        expandedTerm.extend( [' '.join(x)  for x in list(fivegrams)] )
+    else:
+        expandedTerm.extend( [term] )
+
+    if fuzzy_match == True:
+        bigrams = ngrams(term.split(), 2)
+        expandedTerm.extend( [' '.join(x)  for x in list(bigrams)] )
+
+    # for each expanded term, further expanded it for variation
+    for eT in expandedTerm:
+        termVariations.extend( [eT, eT.lower(), eT.rstrip('s'), eT + 's'] )
+
+    return termVariations
