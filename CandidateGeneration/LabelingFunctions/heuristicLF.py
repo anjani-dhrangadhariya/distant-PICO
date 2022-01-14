7797bf761bc3cc787c9b0cbdb3c73d89681da538
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-14 22:15:49 +0100

    Intervention pos regex function complete

--- CandidateGeneration/LabelingFunctions/heuristicLF.py
+++ CandidateGeneration/LabelingFunctions/heuristicLF.py
@@ -1,40 +1,36 @@
 import re
 
-def posPattern_i( text, validation_text_flatten, validation_pos_flatten, spans, lr_window: int = 1 ):
+def posPattern_i( text, validation_text_flatten, validation_pos_flatten, spans, picos: str, lr_window: int = 1 ):
 
     regex_matches = []
 
     for t_i in [  'treatment', 'therapy', 'surgery' ]:
-
         if t_i in text:
             r = re.compile(t_i)
             matches = [m for m in r.finditer(text)]
             regex_matches.extend( matches )
 
     regex_pos_matches = []
-
+    regex_pos_spans = []
+    labels = []
 
     for m in regex_matches:
         if m.span() in spans:
             index = spans.index( m.span() )
 
-            #print( validation_text_flatten[ index-lr_window-1 : index+1  ] )
-            #print( validation_pos_flatten[ index-lr_window-1 : index+1  ] )
+            longest_match = []
+            longest_match_span = []
 
             for i, pos_i in enumerate(reversed( validation_pos_flatten[ :index ] )):
                 if pos_i in ['NN', 'NNS', 'NNP', 'NNS', 'JJ']:
-                    print( validation_text_flatten[ index-i : index+1 ] )
+                    longest_match = validation_text_flatten[ index-i : index+1 ] 
+                    longest_match_span = spans[ index-i : index+1 ] 
                 else:
                     break
 
-
-            
-            '''
-            if any( validation_pos_flatten[ index-2:index+2  ] ) in ['NN', 'NNS', 'NNP', 'NNS', 'JJ']:
-                print( validation_pos_flatten[ index-2:index+2  ] )
-            else:
-                print( 'NO NO' )
-            '''
-                
+            if len( longest_match ) > 1:
+                regex_pos_matches.append( longest_match )
+                regex_pos_spans.append( longest_match_span ) 
+                labels.append( picos )              
     
-    return None
+    return regex_pos_matches, regex_pos_spans, labels
