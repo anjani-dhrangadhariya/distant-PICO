6e036f1fad92d9ba0379373aac82e0afb732c2c0
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-17 11:42:49 +0100

    function to load full EBM PICO

--- CandidateGeneration/load_data.py
+++ CandidateGeneration/load_data.py
@@ -9,7 +9,6 @@ from CandGenUtilities.labeler_utilities import *
 from CandGenUtilities.source_target_mapping import *
 from LabelingFunctions.ontologyLF import *
 from Ontologies.ontologyLoader import *
-from sanity_checks import *
 
 ################################################################################
 # Initialize and set seed
@@ -57,6 +56,46 @@ def load_validation_set(train_dir):
                 corpus_pos.append( [x.strip() for x in sentence_pos ] )
     
     df = pd.DataFrame( {'text': corpus, 'labels': corpus_labels, 'pos': corpus_pos} )
+
+    train, validation = train_test_split(df, test_size=0.20)
+
+    return train, validation
+
+
+def loadEBMPICO(train_dir):
+
+    pmid = []
+    text = []
+    tokens = []
+    pos = []
+    p = []
+    i = []
+    o = []
+
+    with open(f'{train_dir}/result.json', 'r') as rf:
+        data = json.load(rf)
+        for k,v in data.items():
+            pmid.append( k )
+            text.append( [x.strip() for x in v['tokens'] ] )
+            tokens.append( v['tokens'] )
+            pos.append( v['pos'] )
+            if 'participants' in v:
+                p.append( v['participants'] )
+            else:
+                p.append( [ 0 ] * len( v['tokens'] ) )
+
+            if 'interventions' in v:
+                i.append( v['interventions']  )
+            else:
+                i.append( [ 0 ] * len( v['tokens'] ) )
+
+            if 'outcomes' in v:
+                o.append( v['outcomes']  )
+            else:
+                o.append( [ 0 ] * len( v['tokens'] ) )
+    
+
+    df = pd.DataFrame( {'pmid': pmid, 'text': text, 'tokens': tokens, 'pos': pos, 'p': p, 'i': i, 'o': o } )
 
     train, validation = train_test_split(df, test_size=0.20)
 
