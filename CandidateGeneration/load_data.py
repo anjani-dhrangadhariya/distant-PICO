05767f64b440c618cdfd9f9458a70389b5c5d020
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-17 11:43:23 +0100

    removed old load_validation_data function

--- CandidateGeneration/load_data.py
+++ CandidateGeneration/load_data.py
@@ -30,38 +30,6 @@ Args:
 Returns:
     pandas data frame: a pandas dataframe for EBM-NLP training and validation sets
 '''
-def load_validation_set(train_dir):
-    corpus = []
-    corpus_labels = []
-    corpus_pos = []
-
-    with open(f'{train_dir}/{args.entity}/sentence_annotation2POS.txt', 'r') as rf:
-        for eachStudy in rf:
-            data = json.loads(eachStudy)
-            
-            for k,v in data.items():
-
-                sentence_tokens = []
-                sentence_labels = []
-                sentence_pos = []
-
-                for sent_id, sent in v.items():
-
-                    sentence_tokens.extend( sent[0] )
-                    sentence_labels.extend( sent[1] )
-                    sentence_pos.extend( sent[2] )
-
-                corpus.append( [x.strip() for x in sentence_tokens ] )
-                corpus_labels.append( [x.strip() for x in sentence_labels ] )
-                corpus_pos.append( [x.strip() for x in sentence_pos ] )
-    
-    df = pd.DataFrame( {'text': corpus, 'labels': corpus_labels, 'pos': corpus_pos} )
-
-    train, validation = train_test_split(df, test_size=0.20)
-
-    return train, validation
-
-
 def loadEBMPICO(train_dir):
 
     pmid = []
