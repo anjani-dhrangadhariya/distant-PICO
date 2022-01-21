48019fadf9548afaabcc028fe1216d1b50b5ddd6
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-21 13:05:05 +0100

    change in directory structure

---
+++ CandidateGeneration/LabelModelTrain/train_label_model.py
@@ -0,0 +1,101 @@
+import enum
+import os
+import glob
+from pathlib import Path
+import pandas as pd
+
+from LabelModelTrain import LMutils
+
+indir = '/mnt/nas2/results/Results/systematicReview/distant_pico/candidate_generation'
+pathlist = Path(indir).glob('**/*.tsv')
+
+tokens = []
+
+lfs = dict()
+
+for file in pathlist:
+    
+    k = str( file ).split('candidate_generation/')[-1].replace('.tsv', '').replace('/', '_')
+    data = pd.read_csv(file, sep='\t', header=0)
+    if len(tokens) == 0:
+        tokens.extend( list(data.tokens) )
+    
+    lfs[str(k)] = list(data.columns[-1])
+
+
+print( 'Total number of tokens in validation set: ', len(tokens) )
+print( 'Total number of LFs in the dictionary', len(lfs) )
+
+def lf_levels(umls_d:dict, pattern:str, picos:str):
+
+    umls_level = dict()
+
+    for key, value in umls_d.items():   # iter on both keys and values
+        search_pattern = pattern + picos
+        if key.startswith(search_pattern):
+            k = str(key).split('_')[-1]
+            umls_level[ k ] = value
+
+    return umls_level
+
+# Level 1: UMLS
+umls_p = lf_levels(lfs, 'UMLS_fuzzy_', 'p')
+umls_i = lf_levels(lfs, 'UMLS_fuzzy_', 'i')
+umls_o = lf_levels(lfs, 'UMLS_fuzzy_', 'o')
+
+# Level 2: non UMLS
+nonumls_p = lf_levels(lfs, 'nonUMLS_direct_', 'P')
+nonumls_i = lf_levels(lfs, 'nonUMLS_direct_', 'I')
+nonumls_o = lf_levels(lfs, 'nonUMLS_direct_', 'O')
+
+# Level 3: DS
+ds_p = lf_levels(lfs, 'DS_fuzzy_', 'P')
+ds_i = lf_levels(lfs, 'DS_fuzzy_', 'I')
+ds_o = lf_levels(lfs, 'DS_fuzzy_', 'O')
+
+# Level 4: dictionary, rules, heuristics
+heur_p = lf_levels(lfs, 'heuristics_direct_', 'P')
+heur_i = lf_levels(lfs, 'heuristics_direct_', 'I')
+heur_o = lf_levels(lfs, 'heuristics_direct_', 'O')
+
+heur_p = lf_levels(lfs, 'dictionary_direct_', 'P')
+heur_i = lf_levels(lfs, 'dictionary_direct_', 'I')
+heur_o = lf_levels(lfs, 'dictionary_direct_', 'O')
+
+'''#########################################################################
+# Level 1 only
+#########################################################################'''
+
+
+'''#########################################################################
+# Level 1 + Level 2
+#########################################################################'''
+
+
+'''#########################################################################
+# Level 1 + Level 2 + Level 3
+#########################################################################'''
+
+
+'''#########################################################################
+# Level 1 + Level 2 + Level 3 + Level 4
+#########################################################################'''
+
+
+# Rank and partition UMLS SAB's
+ranked_umls_p, partitioned_umls_p = LMutils.rankSAB( umls_p, 'p' )
+ranked_umls_i, partitioned_umls_i = LMutils.rankSAB( umls_i, 'i' )
+ranked_umls_o, partitioned_umls_o = LMutils.rankSAB( umls_o, 'o' )
+
+# for i, partition in enumerate(partitioned_umls_p):
+#     print('Labeling function number: ', len(partition) )
+#     print(partition)
+
+# for i, partition in enumerate(partitioned_umls_i):
+#     print('Labeling function number: ', len(partition) )
+
+# for i, partition in enumerate(partitioned_umls_o):
+#     print('Labeling function number: ', len(partition) )
+
+# Keep the rest stagnant and change UMLS for training PIO * (partition functions) label models
+# build the full pipeline before executing it....
--- CandidateGeneration/Ontologies/ontoUtils.py
+++ CandidateGeneration/Ontologies/ontoUtils.py
@@ -65,55 +65,6 @@ def termCountThreshold(umls_d):
 
     return umls_d
 
-def list2Nested(l, nested_length):
-    return [l[i:i+nested_length] for i in range(0, len(l), nested_length)]
-
-def partitionRankedSAB(umls_d):
-
-    keys = list(umls_d.keys())
-
-    partitioned_lfs = [ ]
-
-    for i in range( 0, len(keys) ):
-
-        if i == 0 or i == len(keys):
-            if i == 0:
-                partitioned_lfs.append( [keys] )
-            if i ==len(keys):
-                temp3 = list2Nested(keys, 1)
-                partitioned_lfs.append( temp3 )
-        else:
-            temp1, temp2 = keys[:i] , keys[i:]
-            temp3 = list2Nested( keys[:i], 1)
-            temp3.append( keys[i:] )
-            partitioned_lfs.append( temp3 )
-
-    return partitioned_lfs
-
-
-def rankSAB(umls_d, picos):
-
-    ranked_umls = []
-    ranked_dict = dict()
-
-    if picos == 'p':
-        ranks_p = open('/home/anjani/distant-PICO/CandidateGeneration/Ontologies/umls_p_rank.txt','r').read()
-        ranked_umls = eval(ranks_p)
-    if picos == 'i':
-        ranks_i = open('/home/anjani/distant-PICO/CandidateGeneration/Ontologies/umls_i_rank.txt','r').read()
-        ranked_umls = eval(ranks_i)
-    if picos == 'o':
-        ranks_o = open('/home/anjani/distant-PICO/CandidateGeneration/Ontologies/umls_o_rank.txt','r').read()
-        ranked_umls = eval(ranks_o)
-
-    for i, l in enumerate(ranked_umls):
-        if l[0] in umls_d:
-            ranked_dict[ l[0] ] = umls_d[l[0]]
-
-    partitioned_umls = partitionRankedSAB(ranked_dict)
-
-    return ranked_dict, partitioned_umls
-
 def removeTerms( umls_d, char_threshold ):
 
     filtered_onto = dict()
