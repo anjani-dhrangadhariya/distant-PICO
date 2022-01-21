aa3de1097dfe7faae20a1525149cd3aca52240d3
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-21 11:25:15 +0100

    read all LF candidates into a dict

--- CandidateGeneration/train_label_model.py
+++ CandidateGeneration/train_label_model.py
@@ -9,22 +9,21 @@ pathlist = Path(indir).glob('**/*.tsv')
 
 tokens = []
 
+lfs = dict()
+
 for file in pathlist:
     
-    k = str( file ).split('candidate_generation/')[-1].replace('.tsv', '')
-    print(k)
-
+    k = str( file ).split('candidate_generation/')[-1].replace('.tsv', '').replace('/', '_')
     data = pd.read_csv(file, sep='\t', header=0)
     if len(tokens) == 0:
         tokens.extend( list(data.tokens) )
+    
+    lfs[str(k)] = list(data.columns[-1])
 
 
 print( 'Total number of tokens in validation set: ', len(tokens) )
+print( 'Total number of LFs in the dictionary', len(lfs) )
 
-
-
-
-# XXX: doing - Read all the labeled candidates into a dictionary
 
 # Combine UMLS candidate labels into partitions
 
