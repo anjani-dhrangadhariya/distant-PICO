6966022433524d7955463d8a53b35ca6d0b43784
anjani-dhrangadhariya <anjaniintern1991@gmail.com>
2022-01-24 18:52:19 +0100

    improved refex for sample size

--- CandidateGeneration/Ontologies/ontologyLoader.py
+++ CandidateGeneration/Ontologies/ontologyLoader.py
@@ -176,7 +176,7 @@ def loadPattern( pattern_name ):
 
     if pattern_name == 'samplesize':
 
-        samp_size_pattern =  r'([0-9]+ ?(patients?|subjects?|participants?|people?|individuals?|persons?|healthy individuals?)+)'
+        samp_size_pattern =  r'([0-9]+ ?(patients?|subjects?|participants?|people?|individuals?|persons?|healthy individuals?|healthy adults?|children?|toddlers?adult?adults?|healthy volunteers?|families?|men?|women?|teenagers?|families?|parturients?)+)'
         compiled_pattern = re.compile(samp_size_pattern)
         return compiled_pattern
 
